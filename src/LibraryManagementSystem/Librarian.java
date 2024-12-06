ff5fbe45699e87174abb1c2a379b0172c9fde429
Velliyangiri, Thiruppathi <Thiruppathi Thiruppathi.Velliyangiri@softwareag.com>
2024-12-06 22:15:51 +0530

    `creating LibraryManagementSystem`

--- src/LibraryManagementSystem/Librarian.java
+++ src/LibraryManagementSystem/Librarian.java
@@ -1,4 +1,25 @@
 package LibraryManagementSystem;
 
-public class Librarian {
+public class Librarian extends User {
+
+    private String employeeNumber;
+
+    @Override
+    public void displayDashboard() {
+        System.out.println("Librarian Dashboard And Employee Number: " + employeeNumber);
+    }
+
+    @Override
+    public boolean canBorrowBooks() {
+        return true;
+    }
+
+    void addNewBook(Book book) {
+
+    }
+
+    void removeBook(Book book) {
+
+    }
+
 }
--- src/LibraryManagementSystem/Member.java
+++ src/LibraryManagementSystem/Member.java
@@ -1,4 +1,35 @@
 package LibraryManagementSystem;
 
-public class Member {
+public class Member extends User {
+    private int borrowedBooksCount;
+    private static int MAX_BORROW_LIMIT = 5;
+
+    @Override
+    public void displayDashboard() {
+        System.out.println("Member Dashboard and Books Borrowed: " + borrowedBooksCount);
+    }
+
+    @Override
+    public boolean canBorrowBooks() {
+        return borrowedBooksCount < MAX_BORROW_LIMIT;
+    }
+
+    public Member() {
+        super();
+    }
+
+    public Member(int borrowedBooksCount) {
+        super();
+        this.borrowedBooksCount = borrowedBooksCount;
+    }
+
+    public Member(String name, String contactInfo, int borrowedBooksCount) {
+        super(name, contactInfo);
+        this.borrowedBooksCount = borrowedBooksCount;
+    }
+
+    public Member(Member other) {
+        super(other);
+        this.borrowedBooksCount = other.borrowedBooksCount;
+    }
 }
